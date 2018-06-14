e2abc36026d4486c083f6c88d56f6235b66b3281
Andrew Corum <14coruma@gmail.com>
2018-06-14 10:55:42 -0600

    * Enable ORB and SIFT as feature extraction methods

--- features.py
+++ features.py
@@ -6,6 +6,18 @@ import progressbar as pb
 import ui
 
 from skimage.feature import local_binary_pattern
+from skimage.feature import ORB
+
+def histogram(image, nBins, range, eps=1e-7):
+    (hist, _) = np.histogram(image.ravel(),
+        bins=np.arange(0, nBins),
+        range=(0, range))
+
+    # normalize the histogram
+    hist = hist.astype("float")
+    hist /= (hist.sum() + eps)
+
+    return hist
 
 class LocalBinaryPatterns:
     def __init__(self, numPoints, radius):
@@ -16,24 +28,20 @@ class LocalBinaryPatterns:
     def describe(self, image, eps=1e-7):
         # compute the LBP representation to build the histogram of patterns
         lbp = local_binary_pattern(image, self.numPoints, self.radius, method="uniform")
-        (hist, _) = np.histogram(lbp.ravel(),
-            bins=np.arange(0, self.numPoints + 3),
-            range=(0, self.numPoints + 2))
- 
-        # normalize the histogram
-        hist = hist.astype("float")
-        hist /= (hist.sum() + eps)
- 
-        # return the histogram of Local Binary Patterns
+        hist = histogram(lbp, self.numPoints+3, self.numPoints+2, eps)
         return hist
 
-def describe_keypoints(img, alg, vector_size, descriptor_size, display):
+def describe_keypoints(img, alg, vector_size, descriptor_size, display=False):
     # Finding image keypoints
-    kps = alg.detect(img)
+    kps = alg.detect(img, None)
 
     # Get first sorted <vector_size> points.
     kps = sorted(kps, key=lambda x: x.response)[:vector_size]
     kps, dsc = alg.compute(img, kps)
+
+    # Fill with zeros if no keypoints are found
+    if len(kps) < vector_size:
+        dsc = np.zeros(shape=(vector_size, descriptor_size))
 
     # Flatten and normalize descriptors
     dsc = dsc.flatten()
@@ -50,8 +58,7 @@ def describe_keypoints(img, alg, vector_size, descriptor_size, display):
 
 # Feature extractor
 def extract_features(images, vector_size=32):
-    # options = ["ORB", "SIFT", "KAZE", "LBP"]
-    options = ["LBP"]
+    options = ["ORB", "SIFT", "LBP"]
     res = ui.prompt("Choose a feature selection algorithm:", options)
     type = options[int(res)]
 
@@ -60,15 +67,11 @@ def extract_features(images, vector_size=32):
         if type == "ORB":              # Corner features
             alg = cv2.ORB_create() 
             descriptor_size = 32
-            data.append(describe_keypoints(img, alg, vector_size, descriptor_size, display))
+            data.append(describe_keypoints(img, alg, vector_size, descriptor_size))
         elif type == "SIFT":           # Corner features (patented)
             alg = cv2.xfeatures2d.SIFT_create() 
             descriptor_size = 128
-            data.append(describe_keypoints(img, alg, vector_size, descriptor_size, display))
-        elif type == "KAZE":           # Corner features (patented)
-            alg = cv2.KAZE_create()
-            descriptor_size = 32
-            data.append(describe_keypoints(img, alg, vector_size, descriptor_size, display))
+            data.append(describe_keypoints(img, alg, vector_size, descriptor_size))
         elif type == "LBP":            # Simple texture recognition
             alg = LocalBinaryPatterns(24, 8)
             grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
