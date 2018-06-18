8c1a96987f5f871cd290fd4e568f80f6b299ce6c
Andrew Corum <14coruma@gmail.com>
2018-06-18 09:31:09 -0600

    - Give user option to apply a new feature extraction method

--- main.py
+++ main.py
@@ -22,17 +22,22 @@ def main():
     dirname = sys.argv[1]
     images, targets = pproc.processPDFs(dirname)
 
-    # Extract feature vectors
-    data = ft.extract_features(images)
+    # Extract feature vectors (until user quits)
+    doneExtracting = False
+    while not doneExtracting:
+        data = ft.extract_features(images)
 
-    # Create, train, and evaluate model (until user quits)
-    done = False
-    while not done:
-        tr.train(data, targets)
-        options = ["Try another model", "Quit"]
-        res = ui.prompt(options=options)
-        if res == "1":
-            done = True
+        # Create, train, and evaluate model (until user quits)
+        doneTraining = False
+        while not doneTraining:
+            tr.train(data, targets)
+            options = ["Try another model", "Extract new features", "Quit"]
+            res = ui.prompt(options=options)
+            if options[int(res)] == "Quit":
+                doneTraining = True
+                doneExtracting = True
+            elif options[int(res)] == "Extract new features":
+                doneTraining = True
 
 if __name__ == "__main__":
     main()
